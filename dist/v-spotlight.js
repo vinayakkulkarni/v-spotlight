23376f281648e448d51c3c9f6af9c308d5361cd6
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-04-19 04:43:29 +0530

    feat(build): upgrade build

--- dist/v-spotlight.js
+++ dist/v-spotlight.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global.VSpotlight = factory());
+  (global = global || self, global.VSpotlight = factory());
 }(this, (function () { 'use strict';
 
   var script = {
@@ -17,164 +17,222 @@
         required: false
       }
     },
-    data: function data() {
+
+    data() {
       return {
         spotlight: null,
         spotlightSize: 'transparent 160px, rgba(0, 0, 0, 0.85) 200px)'
       };
     },
-    mounted: function mounted() {
-      var t = this;
+
+    mounted() {
+      const t = this;
       t.spotlight = document.querySelector('.spotlight');
-      window.addEventListener('mousemove', function (e) {
+      window.addEventListener('mousemove', e => {
         t.updateSpotlight(e);
       });
-      window.addEventListener('mousedown', function (e) {
+      window.addEventListener('mousedown', e => {
         if (e.which === 1) {
           t.spotlightSize = 'transparent 130px, rgba(0, 0, 0, 0.95) 150px)';
           t.updateSpotlight(e);
         }
       });
-      window.addEventListener('mouseup', function (e) {
+      window.addEventListener('mouseup', e => {
         t.spotlightSize = 'transparent 160px, rgba(0, 0, 0, 0.85) 200px)';
         t.updateSpotlight(e);
       });
     },
 
     methods: {
-      updateSpotlight: function updateSpotlight(e) {
-        var t = this;
-        t.spotlight.style.backgroundImage = 'radial-gradient(circle at ' + e.pageX / window.innerWidth * 100 + '% ' + e.pageY / window.innerHeight * 100 + '%, ' + t.spotlightSize;
+      updateSpotlight(e) {
+        const t = this;
+        t.spotlight.style.backgroundImage = `radial-gradient(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 100}%, ${t.spotlightSize}`;
       }
+
     }
   };
 
-  var __vue_script__ = script;
+  function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier /* server only */, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+      if (typeof shadowMode !== 'boolean') {
+          createInjectorSSR = createInjector;
+          createInjector = shadowMode;
+          shadowMode = false;
+      }
+      // Vue.extend constructor export interop.
+      const options = typeof script === 'function' ? script.options : script;
+      // render functions
+      if (template && template.render) {
+          options.render = template.render;
+          options.staticRenderFns = template.staticRenderFns;
+          options._compiled = true;
+          // functional template
+          if (isFunctionalTemplate) {
+              options.functional = true;
+          }
+      }
+      // scopedId
+      if (scopeId) {
+          options._scopeId = scopeId;
+      }
+      let hook;
+      if (moduleIdentifier) {
+          // server build
+          hook = function (context) {
+              // 2.3 injection
+              context =
+                  context || // cached call
+                      (this.$vnode && this.$vnode.ssrContext) || // stateful
+                      (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
+              // 2.2 with runInNewContext: true
+              if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+                  context = __VUE_SSR_CONTEXT__;
+              }
+              // inject component styles
+              if (style) {
+                  style.call(this, createInjectorSSR(context));
+              }
+              // register component module identifier for async chunk inference
+              if (context && context._registeredComponents) {
+                  context._registeredComponents.add(moduleIdentifier);
+              }
+          };
+          // used by ssr in case component is cached and beforeCreate
+          // never gets called
+          options._ssrRegister = hook;
+      }
+      else if (style) {
+          hook = shadowMode
+              ? function (context) {
+                  style.call(this, createInjectorShadow(context, this.$root.$options.shadowRoot));
+              }
+              : function (context) {
+                  style.call(this, createInjector(context));
+              };
+      }
+      if (hook) {
+          if (options.functional) {
+              // register for functional component in vue file
+              const originalRender = options.render;
+              options.render = function renderWithStyleInjection(h, context) {
+                  hook.call(context);
+                  return originalRender(h, context);
+              };
+          }
+          else {
+              // inject component registration as beforeCreate hook
+              const existing = options.beforeCreate;
+              options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+          }
+      }
+      return script;
+  }
 
-  var __vue_render__ = function __vue_render__() {
+  const isOldIE = typeof navigator !== 'undefined' &&
+      /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
+  function createInjector(context) {
+      return (id, style) => addStyle(id, style);
+  }
+  let HEAD;
+  const styles = {};
+  function addStyle(id, css) {
+      const group = isOldIE ? css.media || 'default' : id;
+      const style = styles[group] || (styles[group] = { ids: new Set(), styles: [] });
+      if (!style.ids.has(id)) {
+          style.ids.add(id);
+          let code = css.source;
+          if (css.map) {
+              // https://developer.chrome.com/devtools/docs/javascript-debugging
+              // this makes source maps inside style tags work properly in Chrome
+              code += '\n/*# sourceURL=' + css.map.sources[0] + ' */';
+              // http://stackoverflow.com/a/26603875
+              code +=
+                  '\n/*# sourceMappingURL=data:application/json;base64,' +
+                      btoa(unescape(encodeURIComponent(JSON.stringify(css.map)))) +
+                      ' */';
+          }
+          if (!style.element) {
+              style.element = document.createElement('style');
+              style.element.type = 'text/css';
+              if (css.media)
+                  style.element.setAttribute('media', css.media);
+              if (HEAD === undefined) {
+                  HEAD = document.head || document.getElementsByTagName('head')[0];
+              }
+              HEAD.appendChild(style.element);
+          }
+          if ('styleSheet' in style.element) {
+              style.styles.push(code);
+              style.element.styleSheet.cssText = style.styles
+                  .filter(Boolean)
+                  .join('\n');
+          }
+          else {
+              const index = style.ids.size - 1;
+              const textNode = document.createTextNode(code);
+              const nodes = style.element.childNodes;
+              if (nodes[index])
+                  style.element.removeChild(nodes[index]);
+              if (nodes.length)
+                  style.element.insertBefore(textNode, nodes[index]);
+              else
+                  style.element.appendChild(textNode);
+          }
+      }
+  }
+
+  /* script */
+  const __vue_script__ = script;
+
+  /* template */
+  var __vue_render__ = function() {
     var _vm = this;
     var _h = _vm.$createElement;
     var _c = _vm._self._c || _h;
-    return _c("div", {
-      staticClass: "bg-container",
-      style: {
-        background: "url(" + _vm.background + ") no-repeat " + _vm.position
-      }
-    }, [_c("div", { staticClass: "spotlight" })]);
+    return _c(
+      "div",
+      {
+        staticClass: "bg-container",
+        style: {
+          background: "url(" + _vm.background + ") no-repeat " + _vm.position
+        }
+      },
+      [_c("div", { staticClass: "spotlight" })]
+    )
   };
   var __vue_staticRenderFns__ = [];
   __vue_render__._withStripped = true;
 
-  var __vue_template__ = typeof __vue_render__ !== 'undefined' ? { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ } : {};
+    /* style */
+    const __vue_inject_styles__ = function (inject) {
+      if (!inject) return
+      inject("data-v-788142a2_0", { source: "\n.bg-container[data-v-788142a2] {\n  background-size: cover;\n  height: 100vh;\n}\n.spotlight[data-v-788142a2] {\n  position: absolute;\n  height: 100vh;\n  width: 100vh;\n  background-image: radial-gradient(\n    circle,\n    transparent 160px,\n    rgba(0, 0, 0, 0.85) 200px\n  );\n}\n", map: {"version":3,"sources":["/Users/vinayak/Development/Personal/v-spotlight/src/VSpotlight.vue"],"names":[],"mappings":";AAwDA;EACA,sBAAA;EACA,aAAA;AACA;AACA;EACA,kBAAA;EACA,aAAA;EACA,YAAA;EACA;;;;GAIA;AACA","file":"VSpotlight.vue","sourcesContent":["<template>\n  <div\n    class=\"bg-container\"\n    :style=\"{ background: `url(${background}) no-repeat ${position}` }\"\n  >\n    <div class=\"spotlight\"></div>\n  </div>\n</template>\n<script>\n  export default {\n    name: 'VSpotlight',\n    props: {\n      background: {\n        type: String,\n        required: true,\n      },\n      position: {\n        type: String,\n        default: 'center',\n        required: false,\n      },\n    },\n    data() {\n      return {\n        spotlight: null,\n        spotlightSize: 'transparent 160px, rgba(0, 0, 0, 0.85) 200px)',\n      };\n    },\n    mounted() {\n      const t = this;\n      t.spotlight = document.querySelector('.spotlight');\n      window.addEventListener('mousemove', (e) => {\n        t.updateSpotlight(e);\n      });\n      window.addEventListener('mousedown', (e) => {\n        if (e.which === 1) {\n          t.spotlightSize = 'transparent 130px, rgba(0, 0, 0, 0.95) 150px)';\n          t.updateSpotlight(e);\n        }\n      });\n      window.addEventListener('mouseup', (e) => {\n        t.spotlightSize = 'transparent 160px, rgba(0, 0, 0, 0.85) 200px)';\n        t.updateSpotlight(e);\n      });\n    },\n    methods: {\n      updateSpotlight(e) {\n        const t = this;\n        t.spotlight.style.backgroundImage = `radial-gradient(circle at ${\n          (e.pageX / window.innerWidth) * 100\n        }% ${(e.pageY / window.innerHeight) * 100}%, ${t.spotlightSize}`;\n      },\n    },\n  };\n</script>\n<style scoped>\n  .bg-container {\n    background-size: cover;\n    height: 100vh;\n  }\n  .spotlight {\n    position: absolute;\n    height: 100vh;\n    width: 100vh;\n    background-image: radial-gradient(\n      circle,\n      transparent 160px,\n      rgba(0, 0, 0, 0.85) 200px\n    );\n  }\n</style>\n"]}, media: undefined });
 
-  var __vue_inject_styles__ = function (inject) {
-    if (!inject) return;
-    inject("data-v-4ca9553e_0", { source: "\n.bg-container[data-v-4ca9553e] {\n  background-size: cover;\n  height: 100vh;\n}\n.spotlight[data-v-4ca9553e] {\n  position: absolute;\n  height: 100vh;\n  width: 100vh;\n  background-image: radial-gradient(\n    circle,\n    transparent 160px,\n    rgba(0, 0, 0, 0.85) 200px\n  );\n}\n", map: undefined, media: undefined });
-  };
-
-  var __vue_scope_id__ = "data-v-4ca9553e";
-
-  var __vue_module_identifier__ = undefined;
-
-  var __vue_is_functional_template__ = false;
-
-  function __vue_normalize__(template, style, script$$1, scope, functional, moduleIdentifier, createInjector, createInjectorSSR) {
-    var component = (typeof script$$1 === 'function' ? script$$1.options : script$$1) || {};
-
-    {
-      component.__file = "/Users/vinayak/Development/Personal/v-spotlight/src/VSpotlight.vue";
-    }
-
-    if (!component.render) {
-      component.render = template.render;
-      component.staticRenderFns = template.staticRenderFns;
-      component._compiled = true;
-
-      if (functional) component.functional = true;
-    }
-
-    component._scopeId = scope;
-
-    {
-      var hook = void 0;
-      if (style) {
-        hook = function hook(context) {
-          style.call(this, createInjector(context));
-        };
-      }
-
-      if (hook !== undefined) {
-        if (component.functional) {
-          var originalRender = component.render;
-          component.render = function renderWithStyleInjection(h, context) {
-            hook.call(context);
-            return originalRender(h, context);
-          };
-        } else {
-          var existing = component.beforeCreate;
-          component.beforeCreate = existing ? [].concat(existing, hook) : [hook];
-        }
-      }
-    }
-
-    return component;
-  }
-
-  function __vue_create_injector__() {
-    var head = document.head || document.getElementsByTagName('head')[0];
-    var styles = __vue_create_injector__.styles || (__vue_create_injector__.styles = {});
-    var isOldIE = typeof navigator !== 'undefined' && /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
-
-    return function addStyle(id, css) {
-      if (document.querySelector('style[data-vue-ssr-id~="' + id + '"]')) return;
-
-      var group = isOldIE ? css.media || 'default' : id;
-      var style = styles[group] || (styles[group] = { ids: [], parts: [], element: undefined });
-
-      if (!style.ids.includes(id)) {
-        var code = css.source;
-        var index = style.ids.length;
-
-        style.ids.push(id);
-
-        if (isOldIE) {
-          style.element = style.element || document.querySelector('style[data-group=' + group + ']');
-        }
-
-        if (!style.element) {
-          var el = style.element = document.createElement('style');
-          el.type = 'text/css';
-
-          if (css.media) el.setAttribute('media', css.media);
-          if (isOldIE) {
-            el.setAttribute('data-group', group);
-            el.setAttribute('data-next-index', '0');
-          }
-
-          head.appendChild(el);
-        }
-
-        if (isOldIE) {
-          index = parseInt(style.element.getAttribute('data-next-index'));
-          style.element.setAttribute('data-next-index', index + 1);
-        }
-
-        if (style.element.styleSheet) {
-          style.parts.push(code);
-          style.element.styleSheet.cssText = style.parts.filter(Boolean).join('\n');
-        } else {
-          var textNode = document.createTextNode(code);
-          var nodes = style.element.childNodes;
-          if (nodes[index]) style.element.removeChild(nodes[index]);
-          if (nodes.length) style.element.insertBefore(textNode, nodes[index]);else style.element.appendChild(textNode);
-        }
-      }
     };
-  }
+    /* scoped */
+    const __vue_scope_id__ = "data-v-788142a2";
+    /* module identifier */
+    const __vue_module_identifier__ = undefined;
+    /* functional template */
+    const __vue_is_functional_template__ = false;
+    /* style inject SSR */
+    
+    /* style inject shadow dom */
+    
 
+    
+    const __vue_component__ = normalizeComponent(
+      { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+      __vue_inject_styles__,
+      __vue_script__,
+      __vue_scope_id__,
+      __vue_is_functional_template__,
+      __vue_module_identifier__,
+      false,
+      createInjector,
+      undefined,
+      undefined
+    );
 
-  var VSpotlight = __vue_normalize__(__vue_template__, __vue_inject_styles__, typeof __vue_script__ === 'undefined' ? {} : __vue_script__, __vue_scope_id__, __vue_is_functional_template__, __vue_module_identifier__, typeof __vue_create_injector__ !== 'undefined' ? __vue_create_injector__ : function () {}, typeof __vue_create_injector_ssr__ !== 'undefined' ? __vue_create_injector_ssr__ : function () {});
-
-  return VSpotlight;
+  return __vue_component__;
 
 })));
